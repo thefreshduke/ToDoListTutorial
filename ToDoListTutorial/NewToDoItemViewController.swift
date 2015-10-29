ba536b70d2bee00cff664bfd25136e6e598afa19
thefreshduke <scottyshaw1@gmail.com>
2015-10-29 15:28:29 -0400

    Created NewToDoItermViewController.swift and connected file to storyboard element

---
+++ ToDoListTutorial/NewToDoItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewToDoItemViewController.swift
+//  ToDoListTutorial
+//
+//  Created by Scotty Shaw on 10/29/15.
+//  Copyright © 2015 ___sks6___. All rights reserved.
+//
+
+import UIKit
+
+class NewToDoItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
